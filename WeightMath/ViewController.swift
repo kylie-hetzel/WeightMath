e2f5d90db228177d5ab807cd802f329cf4f100d9
Kylie Hetzel <kyliehetzel98@gmail.com>
2018-08-13 15:09:16 -0700

    Initial Commit

---
+++ WeightMath/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeightMath
+//
+//  Created by Kylie Hetzel on 8/13/18.
+//  Copyright © 2018 Kylie Hetzel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
